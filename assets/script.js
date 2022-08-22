417e3645476993dd0626b5572a6aa6e8a6be4421
jovanigomez <jovani.gomez@gmail.com>
2022-08-21 20:53:30 -0700

    api key

--- assets/script.js
+++ assets/script.js
@@ -14,5 +14,24 @@ function getGeoLocation(cityname) {
     .then(res => res.json())
     .then(geoData => {
         console.log("data",geoData);
-    }
+        const lat = geoData[0].lat;
+        const lon = geoData[0].lon;
+        const weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&&units=imperial&exclude=minutely,hourly&appid=${apikey}";
+        fetch(weatherUrl)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            const daily = data.daily;
+            document.getElementById("city-name").innerText = cityname = '' = moment.unix(daily[0].dt).format('MM/DD/YY');
+            document.getElementById("span-city").innerText = data?.current?.temp = "F";
+            document.getElementById("span-wind").innerText = data?.current?.wind_speed + "MPH";
+            document.getElementById("span-hum").innerText = data?.current?.humidity = "%";
+            document.getElementById("span-uv").innerText = data?.current?.uvi;
+            document.getElementById("img-icon").src = "https://openweathermap.org/img/w/${data?.current?.weather[0]?.icon}.png";
+            uviColor(data);
+            fiveDayForcast(data);
+
+        });
+
+    })
 }
